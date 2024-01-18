e83e791fb6a33a7f55c589b62ca058b1542110a7
Man Tran <man.tm@kisvn.vn>
2024-01-18 09:27:00 +0700

    adding retry

--- src/main/java/vn/kis/ben10/clients/BidvClient.java
+++ src/main/java/vn/kis/ben10/clients/BidvClient.java
@@ -1,5 +1,6 @@
 package vn.kis.ben10.clients;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,12 +12,15 @@ import vn.kis.ben10.models.BidvReqHeaderDTO;
 @FeignClient(value = "bidvClient", url = "${feign.client.config.bidvClient.baseURL}")
 public interface BidvClient {
 
+    @Retry(name = "throwingException1")
     @PostMapping("/getListBank/v002")
     String getBanks(@RequestBody BidvDTO<BidvReqHeaderDTO, BidvGetBanksReqBodyDTO> requestBody);
 
+    @Retry(name = "throwingException1")
     @PostMapping("/getName247/v002")
     String getAccountHolderName247(@RequestBody BidvDTO<BidvReqHeaderDTO, BidvGetName247ReqBodyDTO> requestBody);
 
+    @Retry(name = "throwingException1")
     @PostMapping("/getNameBidv/v002")
     String getAccountHolderNameInBidvBank(@RequestBody BidvDTO<BidvReqHeaderDTO, BidvGetName247ReqBodyDTO> requestBody);
 }
--- src/main/java/vn/kis/ben10/clients/TtlClient.java
+++ src/main/java/vn/kis/ben10/clients/TtlClient.java
@@ -1,5 +1,6 @@
 package vn.kis.ben10.clients;
 
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,9 +12,11 @@ import vn.kis.ben10.models.TtlGetOperatorTokenResDTO;
 @FeignClient(value = "ttlClient", url = "${feign.client.config.ttlClient.baseURL}")
 public interface TtlClient {
 
+    @Retry(name = "throwingException1")
     @PostMapping("/operatorLogin")
     TtlGetOperatorTokenResDTO getOperatorToken(@RequestBody TtlGetOperatorTokenReqDTO requestBody);
 
+    @Retry(name = "throwingException1")
     @PostMapping("/services/eqt/listbankbranch")
     TtlDefinedBanksResDTO getDefinedBanks(@RequestBody TtlDefinedBanksReqDTO requestBody);
 }
